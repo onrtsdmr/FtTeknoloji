616cf6f0dbb0c86dc7e936a69cca3c68d8a07ddc
Onur TAŞDEMİR <47485584+onrtsdmr@users.noreply.github.com>
2024-10-25 17:42:12 +0300

    NewsCurrency Model has been added.

---
+++ app/Models/NewsCurrency.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsCurrency extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'news_id', 'currency_code', 'currency_title', 'currency_slug', 'currency_url'
+    ];
+}
